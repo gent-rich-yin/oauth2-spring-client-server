8c0e8aa99f5b6143be1b481ce69f7dada440b089
Richard Yin <yguobin@hotmail.com>
2022-10-20 07:57:45 -0400

    Added more endpoints

--- src/main/java/org/example/oauth2/client/server/web/ArticlesController.java
+++ src/main/java/org/example/oauth2/client/server/web/ArticlesController.java
@@ -1,10 +1,16 @@
 package org.example.oauth2.client.server.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
@@ -26,5 +32,23 @@ public class ArticlesController {
           .retrieve()
           .bodyToMono(String[].class)
           .block();
+    }
+
+    @GetMapping(value = "greeting")
+    public String getGreeting(@RegisteredOAuth2AuthorizedClient("articles-client-oidc") OAuth2AuthorizedClient oAuth2AuthorizedClient) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + oAuth2AuthorizedClient.getAccessToken().getTokenValue());
+        HttpEntity<String> request = new HttpEntity<>(httpHeaders);
+        return restTemplate.exchange("http://resource-server:8090/greeting", HttpMethod.GET, request, String.class).getBody();
+    }
+
+    @GetMapping(value = "token")
+    public String getToken(@RegisteredOAuth2AuthorizedClient("articles-client-oidc") OAuth2AuthorizedClient oAuth2AuthorizedClient) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, "Bearer " + oAuth2AuthorizedClient.getAccessToken().getTokenValue());
+        HttpEntity<String> request = new HttpEntity<>(httpHeaders);
+        return restTemplate.exchange("http://resource-server:8090/token", HttpMethod.GET, request, String.class).getBody();
     }
 }
